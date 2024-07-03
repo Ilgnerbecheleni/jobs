af541b93f5fc7cf992b925d96e76b3d5b2df01e7
ilgnerbecheleni <ilgner.gui@gmail.com>
2024-07-03 16:26:11 -0300

    http

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://191.101.234.241:3000', // A base URL da sua API
+  baseURL: 'http://191.101.234.241:3000', // A base URL da sua API
  //  baseURL: 'http://localhost:3000'
 });
 
