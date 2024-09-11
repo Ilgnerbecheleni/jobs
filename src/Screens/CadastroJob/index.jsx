2fbcbfa0b3541c980dfe498da5051b5d2ddfec1e
ilgnerbecheleni <ilgner.gui@gmail.com>
2024-09-10 22:23:30 -0300

    cep update

--- src/Screens/CadastroJob/index.jsx
+++ src/Screens/CadastroJob/index.jsx
@@ -1,4 +1,4 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import api from '../../services/api';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -10,10 +10,11 @@ function CadastroJob() {
   const [cep, setCep] = useState('');
   const [localidade, setLocalidade] = useState('');
   const [cepError, setCepError] = useState(false);
+  const [inputError, setInputError] = useState(false);
   const [categorias, setCategorias] = useState([]);
   const [categoriaSelecionada, setCategoriaSelecionada] = useState('');
   const [valorHora, setValorHora] = useState('');
-  const [descricao, setDescricao] = useState(''); // Novo estado para descrição
+  const [descricao, setDescricao] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -24,7 +25,6 @@ function CadastroJob() {
     try {
       const response = await api.get('/categorias');
       setCategorias(response.data);
-      console.log(response.data);
     } catch (error) {
       console.error('Erro ao buscar categorias:', error);
     }
@@ -39,55 +39,65 @@ function CadastroJob() {
           titulo: nome,
           telefone,
           localizacao: localidade,
-          valorHora: parseFloat(valorHora), // Convertendo para float
+          valorHora: parseFloat(valorHora),
           servicoId: categoriaSelecionada,
-          descricao // Incluindo a descrição no objeto enviado
+          descricao
         }
       );
-      console.log('Resposta da API:', response.data);
       setNome('');
       setTelefone('');
       setCep('');
       setLocalidade('');
       setCategoriaSelecionada('');
       setValorHora('');
-      setDescricao(''); // Resetando o estado da descrição
+      setDescricao('');
       navigate('/jobs');
       notfySucess();
     } catch (error) {
       console.error('Erro ao cadastrar job:', error);
-      notifyError(error.response.data.erro);
-      
+      notifyError("Falha ao cadastrar o serviço");
     }
   };
 
   const notifyError = (erro) => {
     toast.error(erro, {
-      position: "top-right", 
+      position: "top-right",
       autoClose: 2000,
       pauseOnFocusLoss: false,
     });
   }
 
-  const notfySucess = ()=>{
-    toast.success("Usuario cadastrado!!", {
-      position: "top-rigth"
+  const notfySucess = () => {
+    toast.success("Usuário cadastrado!!", {
+      position: "top-right"
     });
   }
 
   const handleCepChange = (event) => {
-    const newCep = event.target.value;
+    let newCep = event.target.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+    if (newCep.length > 5) {
+      newCep = newCep.replace(/^(\d{5})(\d{1,3})/, '$1-$2'); // Aplica a máscara
+    }
     setCep(newCep);
-    if (newCep.length === 8) {
+
+    // Verifica se o CEP está completo (8 dígitos)
+    if (newCep.length === 9) {
       fetchCEP(newCep);
     } else {
       setCepError(false);
+      setInputError(false);
+    }
+  };
+
+  const handleCepBlur = () => {
+    if (cep.length !== 9) {
+      setInputError(true);
     }
   };
 
   const fetchCEP = async (cep) => {
     try {
-      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+      const response = await axios.get(`https://viacep.com.br/ws/${cep.replace('-', '')}/json/`);
       if (response.data.localidade) {
         setLocalidade(`${response.data.localidade}, ${response.data.uf}`);
         setCepError(false);
@@ -103,9 +113,7 @@ function CadastroJob() {
 
   return (
     <section className='container mt-5'>
-        
-       <ToastContainer limit={1}/>
-
+      <ToastContainer limit={1} />
       <h3 className='display-4'>Cadastre um Serviço</h3>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
@@ -116,10 +124,19 @@ function CadastroJob() {
           <label htmlFor="telefone" className="form-label">Telefone de Contato</label>
           <input type="tel" className="form-control" id="telefone" value={telefone} onChange={(e) => setTelefone(e.target.value)} required />
         </div>
-        <div className={`mb-3 ${cepError ? 'has-error' : ''}`}>
+        <div className={`mb-3 ${cepError || inputError ? 'has-error' : ''}`}>
           <label htmlFor="cep" className="form-label">CEP</label>
-          <input type="text" className={`form-control ${cepError ? 'is-invalid' : ''}`} id="cep" value={cep} onChange={handleCepChange} required maxLength={8} />
-          {cepError && <div className="invalid-feedback">Erro ao buscar CEP. Por favor, verifique o CEP e tente novamente.</div>}
+          <input
+            type="text"
+            className={`form-control ${cepError || inputError ? 'is-invalid' : ''}`}
+            id="cep"
+            value={cep}
+            onChange={handleCepChange}
+            onBlur={handleCepBlur}
+            maxLength={9} // Para a máscara XXXXX-XXX
+            required
+          />
+          {(cepError || inputError) && <div className="invalid-feedback">Digite um CEP válido (somente números).</div>}
         </div>
         <div className="mb-3">
           <label htmlFor="localidade" className="form-label">Localidade (Cidade)</label>
@@ -140,11 +157,10 @@ function CadastroJob() {
         </div>
         <div className="mb-3">
           <label htmlFor="descricao" className="form-label">Descrição (Opcional)</label>
-          <textarea className="form-control" id="descricao" value={descricao} onChange={(e) => setDescricao(e.target.value)} />
+          <textarea className="form-control" id="descricao" value={descricao} onChange={(e) => setDescricao(e.target.value)} maxLength={20} />
         </div>
         <button type="submit" className="btn btn-primary">Cadastrar Job</button>
       </form>
-     
     </section>
   );
 }
